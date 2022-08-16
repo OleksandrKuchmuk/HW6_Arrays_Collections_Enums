88be2068aa9ee852aaeefdd857ed2d6a57a7cd08
Oleksandr Kuchmuk <96177236+OleksandrKuchmuk@users.noreply.github.com>
2022-08-16 15:16:50 +0300

    Initial commit. Start my progect.

---
+++ src/main/java/com/cursor/Main.java
@@ -0,0 +1,12 @@
+package com.cursor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
